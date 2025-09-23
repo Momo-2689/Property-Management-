166fea18899c9b1349e88af2d4d2de9eab72c970
Terushan <terushanm@gmail.com>
2025-09-23 21:40:38 +0200

    Changes to models.

---
+++ models/tenant.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base   
+
+class Tenant(Base):
+    __tablename__ = "tenant"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    phone = Column(String)
+    property_id = Column(Integer, ForeignKey("properties.id"))
