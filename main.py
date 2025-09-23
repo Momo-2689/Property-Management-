abda16ee22c8d470729969f8678656d9863309fb
Momo-Ally <m.ally.irfaan@gmail.com>
2025-09-23 22:35:59 +0200

    main.py changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from database import Base, engine
+from database import Base, create_engine
 from models import property, tenant , maintenance
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=create_engine)
